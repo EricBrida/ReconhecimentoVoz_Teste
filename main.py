eb1b8030f166397ada40bb0aaa514330b9fca593
EricBrida <ericbrida2@gmail.com>
2024-10-13 14:46:50 -0300

    🚧 : main.py estruturação

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-if __name__ == '__main__':
-    ...
+import speech_recognition as sr
+from app.gui_module import VoiceRecognitionApp
+import tkinter as tk
+
+def main():
+    reconhecedor = sr.Recognizer()
+
+    root = tk.Tk()
+    app = VoiceRecognitionApp(root, reconhecedor)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
